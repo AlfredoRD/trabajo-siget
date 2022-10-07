f040e8c87049365ac2728029d36f0206f0030269
AlfredoRD <61297115+AlfredoRD@users.noreply.github.com>
2022-10-07 15:40:49 -0400

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,28 @@
-import { createStore } from 'vuex'
-import Formulacion from '@/modules/financiero/FormulacionModule/store/Formulacion'
-import AdministrativoModule from '@/modules/administrativo/Usuario/store/Administrativo'
-import auth from '@/modules/auth/store/module-template'
+// import { createStore } from 'vuex'
+// import Formulacion from '@/modules/financiero/FormulacionModule/store/Formulacion'
+// import AdministrativoModule from '@/modules/administrativo/Usuario/store/Administrativo'
+// import auth from '@/modules/auth/store/module-template'
 
-export default createStore({
-  state: {
-    sidebarVisible: '',
-    sidebarUnfoldable: false,
-  },
-  mutations: {
-    toggleSidebar(state) {
-      state.sidebarVisible = !state.sidebarVisible
-    },
-    toggleUnfoldable(state) {
-      state.sidebarUnfoldable = !state.sidebarUnfoldable
-    },
-    updateSidebarVisible(state, payload) {
-      state.sidebarVisible = payload.value
-    },
-  },
-  actions: {},
-  modules: {
-    auth,
-    Formulacion,
-    AdministrativoModule,
-  },
-})
+// export default createStore({
+//   state: {
+//     sidebarVisible: '',
+//     sidebarUnfoldable: false,
+//   },
+//   mutations: {
+//     toggleSidebar(state) {
+//       state.sidebarVisible = !state.sidebarVisible
+//     },
+//     toggleUnfoldable(state) {
+//       state.sidebarUnfoldable = !state.sidebarUnfoldable
+//     },
+//     updateSidebarVisible(state, payload) {
+//       state.sidebarVisible = payload.value
+//     },
+//   },
+//   actions: {},
+//   modules: {
+//     auth,
+//     Formulacion,
+//     AdministrativoModule,
+//   },
+// })
